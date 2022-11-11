292b8a89c320571c4f0824214448a5d9934e0d8c
lazs <37223122+lazs@users.noreply.github.com>
2022-11-11 16:10:14 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,76 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+import numpy as np
+import plotly.graph_objs as go
+
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+st.title("Plotly examples")
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.header("Chart with two lines")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+trace0 = go.Scatter(x=[1, 2, 3, 4], y=[10, 15, 13, 17])
+trace1 = go.Scatter(x=[1, 2, 3, 4], y=[16, 5, 11, 9])
+data = [trace0, trace1]
+st.write(data)
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+###
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+st.header("Matplotlib chart in Plotly")
 
-    points_per_turn = total_points / num_turns
+import matplotlib.pyplot as plt
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+f = plt.figure()
+arr = np.random.normal(1, 1, size=100)
+plt.hist(arr, bins=20)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+st.plotly_chart(f)
+
+
+###
+
+st.header("3D plot")
+
+x, y, z = np.random.multivariate_normal(np.array([0, 0, 0]), np.eye(3), 400).transpose()
+
+trace1 = go.Scatter3d(
+    x=x,
+    y=y,
+    z=z,
+    mode="markers",
+    marker=dict(
+        size=12,
+        color=z,  # set color to an array/list of desired values
+        colorscale="Viridis",  # choose a colorscale
+        opacity=0.8,
+    ),
+)
+
+data = [trace1]
+layout = go.Layout(margin=dict(l=0, r=0, b=0, t=0))
+fig = go.Figure(data=data, layout=layout)
+
+st.write(fig)
+
+
+###
+
+st.header("Fancy density plot")
+
+import numpy as np
+import plotly.figure_factory as ff
+
+# Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
+
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ["Group 1", "Group 2", "Group 3"]
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(hist_data, group_labels, bin_size=[0.1, 0.25, 0.5])
+
+# Plot!
+st.plotly_chart(fig)
